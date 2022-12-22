7655745ea0d4ebff5fd534b94a6d19dff712edae
Sabi Mantock <sabimantock@gmail.com>
2022-12-22 15:19:01 +0000

    list-structure: complete main features: - fixed all linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import './style.css';
+
 const list = document.getElementById('lists');
 const data = [
-  {index: 0, description: 'Wash the dishes', completed: false},
-  {index: 1, description: 'Complete To Do list project', completed: false},
+  { index: 0, description: 'Wash the dishes', completed: false },
+  { index: 1, description: 'Complete To Do list project', completed: false },
 ];
 
-const display = () => {
-  data.map((todo) => addTodoItem(todo));
-};
-
 const addTodoItem = (todo) => {
-  return (list.innerHTML += `
+  list.innerHTML += `
   <li
   id=${todo.index}
   class="listItem"
@@ -23,7 +20,10 @@ const addTodoItem = (todo) => {
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M12.025 21.15q-1 0-1.7-.7t-.7-1.675q0-1 .7-1.7t1.675-.7q1 0 1.688.7.687.7.687 1.7 0 .975-.687 1.675-.688.7-1.663.7Zm0-6.775q-1 0-1.7-.7T9.625 12q0-1 .7-1.688.7-.687 1.675-.687 1 0 1.688.687.687.688.687 1.663 0 1-.687 1.7-.688.7-1.663.7Zm0-6.75q-1 0-1.7-.713-.7-.712-.7-1.687 0-1 .7-1.688.7-.687 1.675-.687 1 0 1.688.687.687.688.687 1.688 0 .975-.687 1.687-.688.713-1.663.713Z" /></svg>
           </li>
-  `);
+  `;
+};
+const display = () => {
+  data.map((todo) => addTodoItem(todo));
 };
 
 document.addEventListener('DOMContentLoaded', display);
